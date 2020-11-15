2e6d37bd7b3919cd1efb0474850b8c58ad8a878c
Harshaldalvi7797 <[dalviharshal7797@gmail.com]>
2020-11-15 23:18:18 +0530

    commit : frontend page done for select file

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
+
+const path = require("path");
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const methodOverride = require("method-override");
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-const port = 5000;
+const port = 3000;
 app.listen(port, () => {
   console.log(`server started on ${port}  successfully`);
 });
